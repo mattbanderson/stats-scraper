21b0766026a7cf941dd73ae38bd3f2c716079e4b
Matt Anderson <matt@mattbanderson.com>
2017-08-15 21:46:05 -0400

    Add example config file.

---
+++ src/config/config-secrets-example.js
@@ -0,0 +1,10 @@
+/*
+Copy this file, rename to config-secrets.js and set the values below as appropriate.
+*/
+
+var secrets = {};
+
+secrets.url = 'http://www.someurl.com';
+secrets.classname = '.classname';
+
+module.exports = secrets;
